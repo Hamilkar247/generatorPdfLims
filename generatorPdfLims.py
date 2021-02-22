bdc4ece168a751b4083c37bba5516c66f2af130c
Hamilkar247 <adonnibal96@gmail.com>
2021-02-22 14:13:46 +0100

    [WO-1] fpdf - pierwsze użycie
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- generatorPdfLims.py
+++ generatorPdfLims.py
@@ -1,5 +1,6 @@
 import logging
 import argparse
+from fpdf import FPDF
 
 def def_params():
     parser = argparse.ArgumentParser(
@@ -12,9 +13,28 @@ def def_params():
         print("args:" + str(args))
     return args
 
+def change_fonts():
+    pdf = FPDF()
+    pdf.add_page()
+    font_size = 8
+    for font in pdf.core_fonts:
+        if any([letter for letter in font if letter.isupper()]):
+            # skip this font
+            continue
+        pdf.set_font(font, size=font_size)
+        txt = "Font name: {} - {} pts".format(font, font_size)
+        pdf.cell(0, 10, txt=txt, ln=1, align="C")
+        font_size += 2 
+    pdf.output("change_fonts.pdf")
 
 def main():
     args=def_params()
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.cell(200, 10, txt="Welcome to Python!", ln=1, align="C")
+    pdf.output("simple_demo.pdf")
+    change_fonts()
 
 if __name__ == "__main__":
     main()
